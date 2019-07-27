5d49507977de6095c077279e6932c17e38ba6715
taehyeong77@naver.com <taehyeong77@naver.com>
2019-07-28 01:32:26 +0900

    commit

---
+++ MFCApplication2/MFCApplication2Dlg.cpp
@@ -0,0 +1,127 @@
+﻿
+// MFCApplication2Dlg.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "MFCApplication2.h"
+#include "MFCApplication2Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication2Dlg 대화 상자
+
+
+
+CMFCApplication2Dlg::CMFCApplication2Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION2_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication2Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication2Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_NCLBUTTONDOWN()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONDOWN()
+END_MESSAGE_MAP()
+
+
+// CMFCApplication2Dlg 메시지 처리기
+
+BOOL CMFCApplication2Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	// 특정 영역만 투명화 해보자.
+	// SetLayeredWindowAttributes(RGB(255, 1, 7), 0, LWA_COLORKEY);
+	// 다이얼로그의 RGB(255, 1, 7)라는 색상을 0 (완전투명)하게 색상기준으로(LWA_COLORKEY) 만들어 달라
+
+	// 다이얼로그의 모든 영역을 투명화 해보자.
+	SetLayeredWindowAttributes(0, 100, LWA_ALPHA);
+	// 투명도 레인지는 0 ~ 255 사이
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCApplication2Dlg::OnPaint()
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+	if (IsIconic())
+	{
+		
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		dc.FillSolidRect(10, 10, 200, 200, RGB(255, 1, 7));
+		
+		// CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCApplication2Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+void CMFCApplication2Dlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	CClientDC dc(this);
+
+	dc.FillSolidRect(point.x - 10, point.y - 10, 20, 20, RGB(255, 1, 7));
+
+
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+
+void CMFCApplication2Dlg::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// Layered 속성을 실시간으로 확인/적용 하고 싶다면?
+	int wnd_style = ::GetWindowLong(m_hWnd, GWL_EXSTYLE);
+
+	if (!(wnd_style & WS_EX_LAYERED)) {
+		// Layered 속성이 안돼어 있다면
+		::SetWindowLong(m_hWnd, GWL_EXSTYLE, wnd_style | WS_EX_LAYERED);
+	}
+
+	CDialogEx::OnRButtonDown(nFlags, point);
+}
